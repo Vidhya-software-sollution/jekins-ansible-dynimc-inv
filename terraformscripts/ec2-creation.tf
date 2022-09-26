607d2cb4ea67c5090c519facb6674ed4cdc66d16
sankarganesh <109961579+Sankarganesh102@users.noreply.github.com>
2022-09-26 19:32:45 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ provider "aws"{
 
 resource "aws_instance" "AWSEC2Instance"{
      count   = 2
-     ami     =  "ami-01216e7612243e0ef"
+     ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-5"]
      key_name        = "devopptraining"
